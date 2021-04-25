0c99eba61b3133bf50742a36b88e1adceb50ab80
Eli Burch <elburch@pdx.edu>
2021-04-24 17:24:44 -0700

    Added things, broken rn most likely

--- client.js
+++ client.js
@@ -1,20 +1,35 @@
 const net = require('net');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 const port = 8080;
 const host = 'localhost';
+
+data = new Object();
 
 const client = new net.Socket();
 
 client.connect({port: port, host: host}, function() {
     console.log("Connected to server\n");
 
-    client.write('Hello, server. ');
+    readline.question('Specify username >> ', (answer) => {
+        data["username"] = answer;
+        console.log(`Set username to ${answer}`);
+        readline.question('Specify room >> ', (answer) => {
+            data["room"] = answer;
+            console.log(`Set room to ${answer}`);
+            client.write(JSON.stringify(data));
+            client.end();
+            readline.close();
+        });
+    });
+
 });
 
 client.on('data', function(chunk) {
     console.log(`Recieved from server: ${chunk.toString()}`);
-
-    client.end();
 });
 
 client.on('end', function() {
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const net = require('net');
 const port = 8080;
 
+users = new Object();
+users["::ffff:50.38.61.81"] = 'Laptop';
+users["::ffff:127.0.0.1"] = 'linux.cecs';
+
 const server = new net.Server();
 
 server.listen(port, function() {
@@ -10,10 +14,12 @@ server.listen(port, function() {
 server.on('connection', function(socket) {
     console.log("Client Connected\n");
 
-    socket.write("Hello from the server\n");
+//    socket.write(`Hello from the server ${users[socket.remoteAddress]}\n`);
 
     socket.on('data', function(chunk) {
-        console.log(`Data from client: ${chunk.toString()}`);
+        console.log(`Data from client: ${JSON.parse(chunk)["username"]}, room ${JSON.parse(chunk)["room"]}`);
+        socket.write(`Hello from the server ${users[socket.remoteAddress]}\n`);
+
     });
 
     socket.on('end', function() {
